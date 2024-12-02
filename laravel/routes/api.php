ccea6ca71ede1e296b9ab1a60efc7f7636f74154
MohammadFakih02 <mohammadfakih025@gmial.com>
2024-12-02 11:33:10 +0200

    added api routes

---
+++ laravel/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\ApiCountMiddleware;
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('user')->middleware(ApiCountMiddleware::class)->group(function () {
+
+    Route::get('/{id?}',[UserController::class,'getUser']);
+    Route::post('/',[UserController::class,'createUser']);
+    Route::put('/',[UserController::class,'updateUser']);
+    Route::delete('/',[UserController::class,'deleteUser']);
+});
+
+Route::prefix('project')->middleware(ApiCountMiddleware::class)->group(function () {
+
+    Route::get('/{id?}',[ProjectController::class,'getProject']);
+    Route::post('/',[ProjectController::class,'createProject']);
+    Route::put('/',[ProjectController::class,'updateProject']);
+    Route::delete('/',[ProjectController::class,'deleteProject']);
+});
